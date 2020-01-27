afb2df69329abe644175b90ec9b4cc0cf5a7299a
alanbraidot <alan_braidot@outlook.com>
2020-01-27 19:28:04 -0300

    Se creo la BottomNavigationToolbar

---
+++ app/src/main/java/com/example/studygroup/ui/create/CreateFragment.java
@@ -0,0 +1,38 @@
+package com.example.studygroup.ui.create;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.studygroup.R;
+
+public class CreateFragment extends Fragment {
+
+    private CreateViewModel mViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        mViewModel =
+                ViewModelProviders.of(this).get(CreateViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_create, container, false);
+        final TextView textView = root.findViewById(R.id.text_create);
+        mViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+}
