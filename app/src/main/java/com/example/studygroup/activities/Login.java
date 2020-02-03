d1c8f719089dc641af4c4fcb2ac3aeaa81d1da42
dinofilippa <dinofilippa99@hotmail.com>
2020-02-03 20:13:01 -0300

    Falta el menu de buscar

---
+++ app/src/main/java/com/example/studygroup/activities/Login.java
@@ -0,0 +1,15 @@
+package com.example.studygroup.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.studygroup.R;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
